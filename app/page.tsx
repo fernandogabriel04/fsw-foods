57d8a92babc4a86b8ef40b031839a1bdc8692b13
Fernando Gabriel <fernando.gabriel322@hotmail.com>
2024-05-11 00:40:59 -0300

    feat: add second promo banner to home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import CategoryList from "./_components/categorylist";
 import Header from "./_components/header";
 import ProductList from "./_components/product-list";
 import Search from "./_components/search";
-import Image from "next/image";
 import { Button } from "./_components/ui/button";
 import { ChevronRightIcon } from "lucide-react";
 import { db } from "./_lib/prisma";
+import PromoBanner from "./_components/promo-banner";
 
 const Home = async () => {
   const products = await db.product.findMany({
@@ -33,14 +33,9 @@ const Home = async () => {
         <CategoryList />
       </div>
       <div className="px-5 pt-6">
-        <Image
+        <PromoBanner
           src="/promo-banner-01.png"
-          alt="Até 30% de desconto em pizzas"
-          height={0}
-          width={0}
-          className="h-auto w-full"
-          sizes="100vw"
-          quality={100}
+          alt="Até 30% de desconto em pizzas!"
         />
       </div>
 
@@ -56,6 +51,13 @@ const Home = async () => {
           </Button>
         </div>
         <ProductList products={products} />
+      </div>
+
+      <div className="px-5 pt-6">
+        <PromoBanner
+          src="/promo-banner-02.png"
+          alt="A partir de R$17,90 em lanches"
+        />
       </div>
     </>
   );
